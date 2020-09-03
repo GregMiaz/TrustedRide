a45c7cf3730a41ee44c75d69491cacd58d1e5e0e
Grzegorz Miazga <greal@wp.pl>
2020-09-03 21:04:35 +0200

    Added ContactController and CustomTagHelper for generating contact emil link

---
+++ TrustedRide.Services/TagHelpers/EmailLinkTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrustedRide.Services.TagHelpers
+{
+    public class EmailLinkTagHelper : TagHelper
+    {
+        public string Email { get; set; }
+        public string Message { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", $"mailto:{Email}");
+
+            output.Content.SetContent(Message);
+        }
+    }
+}
